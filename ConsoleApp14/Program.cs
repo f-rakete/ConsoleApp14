db4febc5a3a73e029cf8832efc912178a84fe6ba
Florian Rakete <gh@rak.dev>
2023-04-19 16:44:54 +0200

    Add project files.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //amazing hangman code here! =)
+            //amazing hangman code here! =)
+            //amazing hangman code here! =)
+            //amazing hangman code here! =)
+            //amazing hangman code here! =)
+            //amazing hangman code here! =)
+            //amazing hangman code here! =)
+            //amazing hangman code here! =)
+
+        }
+    }
+}
