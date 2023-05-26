9a65da8f7fc8471fa3c19de605f81186b4246b0a
Florian Rakete <gh@rak.dev>
2023-05-26 10:09:46 +0200

    Implement double parsing #1

--- ConsoleApp14/Program.cs
+++ ConsoleApp14/Program.cs
@@ -6,14 +6,8 @@
         {
             Console.WriteLine("Hello, World!");
 
-            //amazing hangman code here! =)
-            //amazing hangman code here! =)
-            //amazing hangman code here! =)
-            //amazing hangman code here! =)
-            //amazing hangman code here! =)
-            //amazing hangman code here! =)
-            //amazing hangman code here! =)
-            //amazing hangman code here! =)
+            //Implementation
+
 
         }
     }
